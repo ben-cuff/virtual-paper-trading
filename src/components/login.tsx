195478746d2cb0852ba904cbb0a7b827a513d9fe
ben-cuff <bencuff14@gmail.com>
2024-09-16 10:11:58 -0400

    Update login.tsx

--- src/components/login.tsx
+++ src/components/login.tsx
@@ -3,18 +3,20 @@
 import { useSession, signIn, signOut } from "next-auth/react";
 
 export default function Login() {
-	const { data: sessionData } = useSession();
-	console.log(sessionData);
-	if (!sessionData) {
+	const { data: session } = useSession();
+	if (!session) {
 		return (
 			<div className="">
 				<button onClick={() => signIn()}>Sign In</button>
 			</div>
 		);
 	} else {
+		console.log("Session found:", JSON.stringify(session, null, 2));
 		return (
 			<div className="">
-				<button onClick={() => signOut()}>Sign Out</button>
+				<button onClick={() => signOut()}>
+					Sign Out of {session.user?.name}
+				</button>
 			</div>
 		);
 	}
