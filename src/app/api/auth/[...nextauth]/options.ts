159716e07f889dbfd24d37b1758cca224338e390
ben-cuff <bencuff14@gmail.com>
2024-10-03 16:43:05 -0400

    Added register and login with credentials

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -1,11 +1,35 @@
 import { NextAuthOptions } from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
+import CredentialsProvider from "next-auth/providers/credentials";
 
 export const authOptions: NextAuthOptions = {
 	providers: [
-		GoogleProvider({
-			clientId: process.env.GOOGLE_CLIENT_ID || "",
-			clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
-		}),
-	],
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: {},
+        password: {},
+      },
+      async authorize(credentials, req) {
+		const res = await fetch("http://localhost:8000/login", {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({
+				email: credentials?.email,
+				password: credentials?.password,
+			}),
+		});
+
+		const data = await res.json();
+
+		if (res.ok && data.success) {
+			console.log("login was successful");
+			return data.user;
+		} else {
+			throw new Error(data.message || 'Login failed');
+		}
+      },
+    }),
+  ],
 };
---
+++ src/app/register/page.tsx
@@ -0,0 +1,80 @@
+"use client"
+
+import { useState } from 'react';
+import { signIn, useSession } from 'next-auth/react';
+import { useRouter, redirect } from 'next/navigation';
+
+export default function Register() {
+    const { data: session } = useSession();
+    const router = useRouter();
+
+    if (session) {
+        redirect("/");
+    }
+
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = async (e) => {
+        e.preventDefault(); 
+
+        console.log(name, email, password);
+
+        const response = await fetch("http://localhost:8000/users", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                name: name,
+                email: email,
+                password: password,
+            }),
+        });
+
+        if (!response.ok) {
+            const errorData = await response.json();
+            alert(`Error: ${errorData.message}`);
+            return;
+        }
+
+        router.push('/');
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <div>
+                <label htmlFor="name">Name:</label>
+                <input
+                    type="text"
+                    id="name"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    required
+                />
+            </div>
+            <div>
+                <label htmlFor="email">Email:</label>
+                <input
+                    type="email"
+                    id="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    required
+                />
+            </div>
+            <div>
+                <label htmlFor="password">Password:</label>
+                <input
+                    type="password"
+                    id="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                />
+            </div>
+            <button type="submit">Register</button>
+        </form>
+    );
+}
