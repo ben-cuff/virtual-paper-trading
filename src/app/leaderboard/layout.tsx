ce1c62bc469f9a7e1be5ecba6e850d38b892702a
ben-cuff <bencuff14@gmail.com>
2024-10-07 17:31:29 -0400

    added leaderboard page

---
+++ src/app/leaderboard/layout.tsx
@@ -0,0 +1,11 @@
+export default function LeaderboardLayout({
+	children,
+}: Readonly<{
+	children: React.ReactNode;
+}>) {
+	return (
+		<html lang="en">
+			<body>{children}</body>
+		</html>
+	);
+}
---
+++ src/app/leaderboard/page.tsx
@@ -0,0 +1,10 @@
+import Leaderboard from "@/components/leaderboard";
+
+export default async function LeaderboardPage() {
+	return (
+		<div>
+			<p>Leaderboard Page</p>
+			<Leaderboard />
+		</div>
+	);
+}
