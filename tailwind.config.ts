249a6d5d90ff2d47d2c803874235f83eb719d8d8
ben-cuff <bencuff14@gmail.com>
2024-10-13 16:36:18 -0400

    added a global background

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,12 @@
 module.exports = {
-	content: [
-		"./src/**/*.{js,jsx,ts,tsx}",
-	],
+	content: ["./src/**/*.{js,jsx,ts,tsx}"],
 	theme: {
-		extend: {},
+		extend: {
+			backgroundImage: {
+				"custom-gradient":
+					"linear-gradient(120deg, #1f2937 0%, #2d3748 100%)",
+			},
+		},
 	},
 	plugins: [],
 };
