34eebfd44ddcb36cdba723059941ba1305ed4b91
ben-cuff <bencuff14@gmail.com>
2024-10-14 19:51:31 -0400

    started leaderboard page, getting unusual hydration error. will fix later

--- src/app/leaderboard/page.tsx
+++ src/app/leaderboard/page.tsx
@@ -1,10 +1,100 @@
-import Leaderboard from "@/components/leaderboard";
+"use client";
 
-export default async function LeaderboardPage() {
+import getLeaderboard from "@/util/get-leaderboard";
+import { useEffect, useState } from "react";
+
+interface LeaderboardEntry {
+	name: string;
+	total_worth: number;
+}
+
+export default function LeaderboardPage() {
+	const [leaderboard, setLeaderboard] = useState([] as LeaderboardEntry[]);
+	const [loading, setLoading] = useState(true);
+
+	useEffect(() => {
+		async function getData() {
+			try {
+				const response = await getLeaderboard();
+				setLeaderboard(response.leaderboard);
+			} catch (error) {
+				console.error("Error fetching leaderboard data:", error);
+			} finally {
+				setLoading(false);
+			}
+		}
+		getData();
+	}, []);
+
+	if (loading) {
+		return (
+			<div className="p-4">
+				<h1 className="text-2xl font-bold mb-4 text-gray-300">
+					Leaderboard (Loading...)
+				</h1>
+				<table className="min-w-full bg-white shadow-md rounded-lg overflow-hidden">
+					<thead className="bg-gray-200">
+						<tr>
+							<th className="py-2 px-4 border-b text-left">
+								Rank
+							</th>
+							<th className="py-2 px-4 border-b text-left">
+								Name
+							</th>
+							<th className="py-2 px-4 border-b text-right">
+								Total Worth
+							</th>
+						</tr>
+					</thead>
+					<tbody>
+						{Array.from({ length: 5 }).map((_, index) => (
+							<tr key={index} className="animate-pulse">
+								<td className="py-2 px-4 border-b text-left">
+									<div className="h-4 bg-gray-300 rounded w-3/4"></div>
+								</td>
+								<td className="py-2 px-4 border-b">
+									<div className="h-4 bg-gray-300 rounded w-3/4"></div>
+								</td>
+								<td className="py-2 px-4 border-b text-right">
+									<div className="h-4 bg-gray-300 rounded w-3/4"></div>
+								</td>
+							</tr>
+						))}
+					</tbody>
+				</table>
+			</div>
+		);
+	}
+
 	return (
-		<div>
-			<p>Leaderboard Page</p>
-			<Leaderboard />
+		<div className="p-4">
+			<h1 className="text-2xl font-bold mb-4 text-gray-300">
+				Leaderboard:
+			</h1>
+			<table className="min-w-full bg-white shadow-md rounded-lg overflow-hidden">
+				<thead className="bg-gray-200">
+					<tr>
+						<th className="py-2 px-4 border-b text-left">Rank</th>
+						<th className="py-2 px-4 border-b text-left">Name</th>
+						<th className="py-2 px-4 border-b text-right">
+							Total Worth
+						</th>
+					</tr>
+				</thead>
+				<tbody>
+					{leaderboard.slice(0, 100).map((entry, index) => (
+						<tr key={entry.name} className="hover:bg-gray-100">
+							<td className="py-2 px-4 border-b text-left">
+								{index + 1}
+							</td>
+							<td className="py-2 px-4 border-b">{entry.name}</td>
+							<td className="py-2 px-4 border-b text-right">
+								{entry.total_worth}
+							</td>
+						</tr>
+					))}
+				</tbody>
+			</table>
 		</div>
 	);
 }
--- src/components/navigation/navbar.tsx
+++ src/components/navigation/navbar.tsx
@@ -1,61 +1,54 @@
 "use client";
 
-import { useSession } from "next-auth/react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import Login from "./login";
 import Logout from "./logout";
 
 export default function NavBar() {
-	const { data: session } = useSession();
 	const currentRoute = usePathname();
 
-	if (session) {
-		return (
-			<nav className="shadow-md py-2">
-				<div className="flex flex-col md:flex-row items-center px-4 py-2 md:py-0">
-					<div className="text-2xl md:text-3xl font-bold text-white hover:text-gray-400 transition duration-200 ease-in-out mb-4 md:mb-0">
-						<Link href="/">Paper Trading</Link>
-					</div>
-					<div className="flex-grow flex justify-center md:justify-start ml-20">
-						<div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-10">
-							<Link
-								href="/portfolio"
-								className={`relative inline-flex items-center p-2 md:p-4 justify-center rounded-md text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none ${
-									currentRoute === "/portfolio"
-										? "border-b-2 border-white"
-										: ""
-								}`}
-							>
-								Portfolio
-							</Link>
-							<Link
-								href="/transactions"
-								className={`relative inline-flex items-center p-2 md:p-4 justify-center rounded-md text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none ${
-									currentRoute === "/transactions"
-										? "border-b-2 border-white"
-										: ""
-								}`}
-							>
-								Transactions History
-							</Link>
-							<Link
-								href="/leaderboard"
-								className={`relative inline-flex items-center p-2 md:p-4 justify-center rounded-md text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none ${
-									currentRoute === "/leaderboard"
-										? "border-b-2 border-white"
-										: ""
-								}`}
-							>
-								Leaderboard
-							</Link>
-						</div>
-					</div>
-					<Logout />
+	return (
+		<nav className="shadow-md py-2">
+			<div className="flex flex-col md:flex-row items-center px-4 py-2 md:py-0">
+				<div className="text-2xl md:text-3xl font-bold text-white hover:text-gray-400 transition duration-200 ease-in-out mb-4 md:mb-0">
+					<Link href="/">Paper Trading</Link>
 				</div>
-			</nav>
-		);
-	} else {
-		<Login />;
-	}
+				<div className="flex-grow flex justify-center md:justify-start ml-20">
+					<div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-10">
+						<Link
+							href="/portfolio"
+							className={`relative inline-flex items-center p-2 md:p-4 justify-center rounded-md text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none ${
+								currentRoute === "/portfolio"
+									? "border-b-2 border-white"
+									: ""
+							}`}
+						>
+							Portfolio
+						</Link>
+						<Link
+							href="/transactions"
+							className={`relative inline-flex items-center p-2 md:p-4 justify-center rounded-md text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none ${
+								currentRoute === "/transactions"
+									? "border-b-2 border-white"
+									: ""
+							}`}
+						>
+							Transactions History
+						</Link>
+						<Link
+							href="/leaderboard"
+							className={`relative inline-flex items-center p-2 md:p-4 justify-center rounded-md text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none ${
+								currentRoute === "/leaderboard"
+									? "border-b-2 border-white"
+									: ""
+							}`}
+						>
+							Leaderboard
+						</Link>
+					</div>
+				</div>
+				
+			</div>
+		</nav>
+	);
 }
