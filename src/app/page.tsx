e405691daff666c3212ea5088829a6a244ab4127
Ben Cuff <bencuff14@gmail.com>
2024-10-16 22:34:36 -0400

    Refactor loading components and improve code organization

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import LoadingPortfolio from "@/components/loading/loading-portfolio";
+import LoadingTransactions from "@/components/loading/loading-transactions";
 import Portfolio from "@/components/portfolio";
 import Transact from "@/components/transact";
 import Transactions from "@/components/transactions";
@@ -11,7 +13,7 @@ export default async function Home() {
 	if (session) {
 		return (
 			<div className="flex flex-col h-screen">
-				<div className="flex flex-1">
+				<div className="flex">
 					<div className="w-1/4">
 						<Suspense fallback={<div>Loading Transact...</div>}>
 							<Transact id={session.user.id as number} />
@@ -19,13 +21,13 @@ export default async function Home() {
 					</div>
 					<div className="w-1/2"></div>
 					<div className="w-1/4">
-						<Suspense fallback={<div>Loading Transactions...</div>}>
+						<Suspense fallback={<LoadingTransactions />}>
 							<Transactions id={session.user.id as number} />
 						</Suspense>
 					</div>
 				</div>
-				<div className="w-full">
-					<Suspense fallback={<div>Loading Portfolio...</div>}>
+				<div className="flex-1 w-full">
+					<Suspense fallback={<LoadingPortfolio />}>
 						<Portfolio id={session.user.id as number} />
 					</Suspense>
 				</div>
---
+++ src/components/loading/loading-transactions.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const LoadingTransactions: React.FC = () => {
+	return (
+		<div className="p-4 shadow-md border-gray-400 text-white h-96 overflow-y-auto">
+			<h2 className="text-xl font-bold mb-2 text-center">Transactions</h2>
+			<ul className="space-y-2">
+				{Array.from({ length: 5 }).map((_, index) => (
+					<li
+						key={index}
+						className="py-2 px-4 bg-gray-700 rounded-lg shadow-sm text-sm animate-pulse"
+					>
+						<div className="h-4 bg-gray-600 rounded w-3/4 mb-2"></div>
+						<div className="h-4 bg-gray-600 rounded w-1/2"></div>
+					</li>
+				))}
+			</ul>
+		</div>
+	);
+};
+
+export default LoadingTransactions;
