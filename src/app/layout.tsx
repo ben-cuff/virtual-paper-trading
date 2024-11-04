a1a19daddd175d605b0ced2586b05f0e048dbe73
ben-cuff <bencuff14@gmail.com>
2024-11-04 15:11:39 -0500

    trying overflow-y-hidden

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
-			<body className="bg-custom-gradient bg-scroll">
+			<body className="bg-custom-gradient bg-fixed overflow-y-hidden">
 				<ClientProvider>
 					<div className="sticky top-0 z-50 bg-transparent backdrop-filter backdrop-blur-lg">
 						<NavBar />
